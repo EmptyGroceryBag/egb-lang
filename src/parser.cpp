0132e392efa2fe695dd8857521a02a7686f40c94
Joey Meisner <emptygrocerybag@hotmail.com>
2021-11-25 13:52:28 -0500

    Use GNU GPLv3 license.
    Additionally, all future commits by Joey Meisner will be authored by
    "Joey Meisner" whose email is "emptygrocerybag@hotmail.com"

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,3 +1,22 @@
+/*
+Copyright 2021 Joey Meisner
+
+This file is part of egb-lang.
+
+egb-lang is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+egb-lang is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along
+with egb-lang.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include <fstream>
 
@@ -15,16 +34,17 @@ int main(int argc, char* argv[]){
 	if(argc < 2){
 		std::cerr << "Error: No input files specified" << std::endl;
 		return 1;
-	}else{
-		std::ios_base::iostate exception_mask = ifs.exceptions() | std::ios::failbit;
-		ifs.exceptions(exception_mask);
-		try{
-			ifs.open(argv[1], std::ios_base::in);
-		}catch (std::ios_base::failure& e){
-			std::cerr << "Could not open file " << argv[1] << std::endl
-				<< e.what() << std::endl << e.code().value();
-			return e.code().value();
-		}
+	}
+
+	std::ios_base::iostate exception_mask = ifs.exceptions() | std::ios::failbit;
+	ifs.exceptions(exception_mask);
+
+	try{
+		ifs.open(argv[1], std::ios_base::in);
+	}catch (std::ios_base::failure& e){
+		std::cerr << "Could not open file " << argv[1] << std::endl
+			<< e.what() << std::endl << e.code().value();
+		return e.code().value();
 	}
 
 	std::cout << "Opened " << argv[1] << std::endl;
