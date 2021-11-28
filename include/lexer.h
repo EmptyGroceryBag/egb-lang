23733e119a627d70d8e0da92976701c931e330a7
emptygrocerybag <emptygrocerybag@hotmail.com>
2021-11-28 16:29:56 -0500

    Fix `enum class Token` type safety
    The prototype for `get_tok()` used and `int` as its return type when it
    should've been `Token`. This casued strange issues with MSVC.
    
    Additionally, `next_token` was of type `int` when it should've been
    `Token`.
    
    Fixes #8

--- include/lexer.h
+++ include/lexer.h
@@ -1,6 +1,15 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-int get_token(std::FILE*);
+enum class Token{
+	tok_eof = -1,
+	tok_def = -2,
+	tok_extern = -3,
+	tok_identifier = -4,
+	tok_number = -5,
+	tok_undefined = -6
+};
+
+Token get_token(std::FILE*);
 
 #endif
